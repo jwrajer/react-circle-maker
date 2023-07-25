3417f7ceb00b984bbbd9562957c16af5c6cc6578
Wyatt Rajer <wyattrajer@gmail.com>
2023-07-25 12:56:00 -0400

    setup CDNs for React

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-console.log(`it works`);
+const container = document.querySelector(`#root`);
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(container);
+root.render(<App />)
