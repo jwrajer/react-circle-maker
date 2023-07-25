ff254800277ab70854a694a7b9dbc75ec97c238f
Wyatt Rajer <wyattrajer@gmail.com>
2023-07-25 16:13:47 -0400

    completely missed the mark or point of this one

--- scripts.js
+++ scripts.js
@@ -1,11 +1,46 @@
 const container = document.querySelector(`#root`);
 
+// create divs for circle, divs for square
+
 const App = () => {
+
+  // const [color, setColor] = React.useState(0);
+
+  // const getColor = React.useEffect(() => {
+  //   return setColor(Math.floor(Math.random()*255));
+  // }, [])
+  // console.log(color);
+
+  
+
   return (
     <>
-      <h1>Hello World</h1>
+      <Square />
+      <Square />
+      <Square />
+      <Circle />
+      <Circle />
+      <Circle />
     </>
   )
+}
+
+
+
+
+
+const colorPicker = () => {
+  return Math.floor(Math.random() * 4);
+}
+
+const Square = () => {
+  const square = `square ${colorPicker()}`;
+  return <section className={square}></section>
+}
+
+const Circle = () => {
+  const circle = `circle ${colorPicker()}`;
+  return <section className={circle}></section>
 }
 
 const root = ReactDOM.createRoot(container);
